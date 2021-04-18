f23c1fc732c37bb46f838cbb1e7e92f54e19442b
David Augusto <daviaugusto222@gmail.com>
2021-04-18 18:46:41 -0300

    Finish

--- Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Introduction.playgroundpage/main.swift
+++ Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Introduction.playgroundpage/main.swift
@@ -1,3 +1,20 @@
+/*:
+ # About me 👨🏽‍💻
+ Hi, my name is David. I'm 24 and majoring in Computer Engineering besides being part of the Apple Developer Academy in Fortaleza, Brazil. I love programming, technology; illustration and design are my favorite hobbies. This project brings together a bit of everything I love: codes, cute illustration and a little bit of music.
+ 
+ # "What planet is that?" 🪐
+
+ Creativity is a fundamental tool for learning and children have plenty of it. My five years old nephew asks me about everything ("What is this?", "What is that?") and those are a lot of questions for a single child! I don't know where he gets all this from, but the idea for this playground came up with one of these questions. He asked “What planet is that?” when he saw Jupiter in a book and an idea came to mind!
+
+ My nephew is learning the letters and its sounds, but it is hard to teach him at home with so many distractions. I was looking for an idea of project to submit at the WWDC, so I put everything together and this project was born.
+
+ I decided to create a game to teach my nephew about the planets' names. At the beginning that was the whole concept, but he ended up telling me that music could make the game cooler, because he loves music. So now, the planets play together and the sun is its maestro.
+
+ Well, now it's time to explore this "musical" Solar System!
+ 
+ [Next Topic](@next)
+ */
+
 //#-hidden-code
 //
 //  See LICENSE folder for this template’s licensing information.
@@ -5,20 +22,10 @@
 //  Abstract:
 //  The Swift file containing the source code edited by the user of this playground book.
 //
-
-
 import PlaygroundSupport
 import SpriteKit
 import UIKit
 import BookCore
-
-
-
-import PlaygroundSupport
-import SpriteKit
-import UIKit
-import BookCore
-
 //let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: 512, height: 768))
 //if let scene = PlanetsScene(fileNamed: "PlanetsScene") {
 //    scene.scaleMode = .aspectFill
@@ -31,9 +38,6 @@ PlaygroundPage.current.navigateTo(page: .next)
 //PlaygroundPage.current.needsIndefiniteExecution = true
 //#-end-hidden-code
 
-/*:
- 
- 
- */
+
 
 
